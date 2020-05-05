99b91aa9fa40893a3a57a3b108fbc94b463c52c9
Chanhee Kim <chanqqq@gmail.com>
2020-05-05 09:06:30 +0900

    #6.10

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,36 +1,108 @@
-import routes from "../routes"
+import passport from "passport";
+import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
-    res.render("join", {pageTitle: "Join"});
+  res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = (req, res) => {
-    const{
-        body: {name, email, password, password2}
-    } = req;
-    if(password !== password2) {
-        res.status(400);
-        res.render("join", {pageTitle: "Join"});    
-    } else {
-        //To Do : Register User   
-        //To Do : Log in user  
-        res.redirect(routes.home);
+export const postJoin = async (req, res, next) => {
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    try {
+      const user = await User({
+        name,
+        email,
+      });
+      await User.register(user, password);
+      next();
+    } catch (error) {
+      console.log(error);
+      res.redirect(routes.home);
     }
-}
+  }
+};
 
-export const getLogin = (req, res) => res.render("login", {pageTitle: "Log In"});
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
 
-export const postLogin = (req, res) => {
-    res.redirect(routes.home);
-}
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home,
+});
+
+export const githubLogin = passport.authenticate("github");
+
+export const githubLoginCallback = async (_, __, profile, cb) => {
+  const {
+    _json: { id, avatar_url, name, email },
+  } = profile;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      user.githubId = id;
+      user.save();
+      return cb(null, user);
+    }
+    const newUser = await User.create({
+      email,
+      name,
+      githubId: id,
+      avatarUrl: avatar_url,
+    });
+    return cb(null, newUser);
+  } catch (error) {
+    return cb(error);
+  }
+};
+
+export const postGithubLogIn = (req, res) => {
+  res.redirect(routes.home);
+};
+
+export const facebookLogin = passport.authenticate("facebook");
+
+export const facebookLoginCallback = (
+  accessToken,
+  refreshToken,
+  profile,
+  cb
+) => {
+  console.log(accessToken, refreshToken, profile, cb);
+};
+
+export const postFacebookLogin = (req, res) => {
+  res.redirect(routes.home);
+};
 
 export const logout = (req, res) => {
-    // To Do: Process Log Out
-    res.redirect(routes.home);
+  req.logout();
+  res.redirect(routes.home);
 };
 
-export const userDetail = (req, res) => res.render("userDetail", {pageTitle: "User Detail"});
+export const getMe = (req, res) => {
+  res.render("userDetail", { pageTitle: "User Detail", user: req.user });
+};
 
-export const editProfile = (req, res) => res.render("editProfile", {pageTitle: "Edit Profile"});
+export const userDetail = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    const user = await User.findById(id);
+    res.render("userDetail", { pageTitle: "User Detail", user });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
 
-export const changePassword = (req, res) => res.render("changePassword", {pageTitle: "Change Password"});
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
